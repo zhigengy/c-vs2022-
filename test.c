7c5245c5c9fbdda201175039d89df2f9bd2cf574
zhigengy <3384742448@qq.com>
2025-12-10 18:06:46 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1014,3 +1014,67 @@ int main() {
 //    return 0;
 //}
 
+//任意类型的数据排序
+//#include<stdlib.h>
+//
+////void qsort(void* base//要比的数据起始位置,size_t num//元素个数,size_t width//数据元素大小 占几字节,int(* cmp)(const void* e1,const void* e2)//函数指针 比较函数
+////可排序任意类型
+//
+//int cmp_int(const void* e1, const void* e2) {
+//	return (*(int*)e1 - *(int*)e2);
+//}
+//
+//void swap(char* e1, char* e2, int width) {
+//	int i = 0;
+//	for (i = 0; i < width; i++) {
+//		char tmp = *e1;
+//		*e1 = *e2;
+//		*e2 = tmp;
+//		e1++;
+//		e2++;
+//	}
+//}
+//void bubble_sort(void* base, int sz, int width, int(*cmp)(const void* e1, const void* e2)) {
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < sz - 1; i++) {
+//		for (j = 0; j < sz - 1 - i; j++) {
+//			if (cmp((char*)base+j*width, (char*)base + (j+1) * width)>0) {
+//				swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+//			}
+//		}
+//	}
+//}
+//
+//
+//int main() {
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//qsort(arr, sz, sizeof(arr[0]), cmp_int); 
+//	//设计一个类似的
+//	bubble_sort(arr,sz, sizeof(arr[0]), cmp_int);
+//	int i = 0;
+//	for (i = 0; i < sz; i++) {
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//简单递归
+//#include <stdio.h>
+//main()
+//{
+//    char* a = "1234";
+//    fun(a);
+//    printf("\n");
+//}
+//
+//fun(char* s)
+//{
+//    char t;
+//    if (*s) {
+//        t = *s++; fun(s);
+//   
+//        if (t != '\0') putchar(t); }
+//    
+//}
