4958d65437814bc70e21a2a7e10254fcefaf92e4
zhigengy <3384742448@qq.com>
2026-04-13 18:56:28 +0800

    Update Data Structure.c

--- Data Structure.c
+++ Data Structure.c
@@ -69,210 +69,173 @@
 //}
 
 ////单链表
-//#include<stdlib.h>
-//typedef struct LNode {
-//	int num;
-//	struct LNode* next;
-//}LNode,*LinkList;
-//int initLNode(LinkList*);//初始化
-//LinkList ListTailInsert(LinkList* L);//尾插法
-//LinkList ListHeadInsert(LinkList* L);//头插法
-//int insertList(LinkList, int, int);//插入在第i位
-//int insertPriorList(LNode*, int);//在前一位插入
-//int deleteList(LinkList, int, int*);//删除第i位
-//int deleteNode(LNode* );//定点删除
-//LNode* GetElem(LinkList, int);//按位查找
-//LNode* LocateElem(LinkList, int);//按值查找
-//int main() {
-//	LinkList L = NULL; 
-//	return 0;
-//}
-//int initLNode(LinkList* L) {
-//	*L = (LinkList)malloc(sizeof(LNode));
-//	if (*L == NULL) {
-//		return 1;
-//	}
-//	(*L)->num = 0;
-//	(*L)->next = NULL;
-//	return 0;
-//}
-//
-//LinkList ListTailInsert(LinkList* L) {
-//	(*L) = (LinkList)malloc(sizeof(LNode));
-//	if ((*L) == NULL) {
-//		return NULL;
-//	}
-//	(*L)->num = 0;
-//	(*L)->next = NULL;
-//	int x = 0;
-//	if (scanf("%d", &x) != 1) {
-//		return NULL;
-//	}
-//	LNode* s = (*L);
-//	LNode* r = (*L);
-//	while (x != 9999) {
-//		s = (LNode*)malloc(sizeof(LNode));
-//		if (s == NULL) {
-//			return (*L);;
-//		}
-//		s->num = x;
-//		r->next = s;
-//		r = s;
-//		if (scanf("%d", &x) != 1) {
-//			return (*L);
-//		}
-//		r->next = NULL;//zhongyao
-//	}
-//	return (*L);
-//}
-//
-//LinkList ListHeadInsert(LinkList* L) {
-//	(*L) = (LinkList)malloc(sizeof(LNode));
-//	if ((*L) == NULL) {
-//		return NULL;
-//	}
-//	(*L)->num = 0;
-//	(*L)->next = NULL;
-//	LNode* s = (*L);
-//	int x = 0;
-//	if (scanf("%d", &x) != 1) {
-//		return NULL;
-//	}
-//	while (x != 9999) {
-//		s = (LNode*)malloc(sizeof(LNode));
-//		if (s == NULL) {
-//			return (*L);
-//		}
-//		s->num = x;
-//		s->next = (*L)->next;
-//		(*L)->next = s;
-//		if (scanf("%d", &x) != 1) {
-//			return (*L);
-//		}
-//	}
-//	return (*L);
-//}
-//
-//int insertList(LinkList L, int i, int e) {
-//	if (i < 0) {
-//		return 1;
-//	}
-//	//找到第i-1个节点
-//	LNode* p = L;
-//	int j = 0;
-//	while (p != NULL && j < i - 1) {
-//		p = p->next;
-//		j++;
-//	}
-//	//在节点后插入一个新节点
-//	if (p == NULL) {
-//		return 1;
-//	}
-//	LNode* s = (LNode*)malloc(sizeof(LNode));
-//	if (s == NULL) {
-//		return 1;
-//	}
-//	s->num = e;
-//	s->next = p->next;
-//	p->next = s;
-//	return 0;
-//	////后插新节点可封装为一个函数
-//	////即
-//	//int insertNextList(LNode * p, int e) {
-//	//	if (p == NULL) {
-//	//		return 1;
-//	//	}
-//	//	LNode* p2 = (LNode*)malloc(sizeof(LNode));
-//	//	if (p2 == NULL) {
-//	//		return 1;
-//	//	}
-//	//	p2->next = p->next;
-//	//	p2->num = e;
-//	//	p->next = p2;
-//	//	return 0;
-//	//}
-//}
-//
-//int insertPriorList(LNode* p, int e) {
-//	LNode* temp = (LNode*)malloc(sizeof(LNode));
-//	//创建新节点t 把p的数据放入t中 把t的数据放入p中
-//	if (temp == NULL) {
-//		return 1;
-//	}
-//	temp->next = p->next;
-//	temp->num = p->num;
-//	p->next = temp;
-//	p->num = e;
-//	return 0;
-//}
-//
-//int deleteList(LinkList L, int i, int* e) {
-//	if (i < 0) {
-//		return 1;
-//	}
-//	int j = 0;
-//	LNode* p = L;
-//	while (p != NULL && j < i - 1) {
-//		p = p->next;
-//		j++;
-//	}
-//	if (p == NULL) {
-//		return 1;
-//	}
-//	if (p->next == NULL) {
-//		return 1;
-//	}
-//	LNode* p2 = p->next;
-//	p->next = p2->next;
-//	*e = p2->num;
-//	free(p2);
-//	return 0;
-//	////若不带头节点
-//	//if (i == 1) {
-//	//	//此时传参要传LinkList* L否则无法改变L所指向
-//	//	LNode* p1 = *L;
-//	//	*e = p1->num;
-//	//	*L = p1->next;
-//	//	free(p1);
-//	//	return 0;
-//	//}//其他情况与上方一样
-//}
-//
-//int deleteNode(LNode* p) {
-//	//把后继节点数据放入其中删去后继节点
-//	if (p == NULL) {
-//		return 1;
-//	}
-//	if (p->next == NULL) {
-//		return 1;
-//	}
-//	LNode* q = p->next;
-//	p->num = q->num;
-//	p->next = q->next;
-//	free(q);
-//	return 0;
-//}
-//
-//LNode* GetElem(LinkList L, int i) {
-//	if (i < 0) {
-//		return NULL;
-//	}
-//	int j = 0;
-//	LNode* p = L;
-//	while (p != NULL && j < i) {
-//		p = p->next;
-//		j++;
-//	}
-//	return p;
-//}
-//
-//LNode* LocateElem(LinkList L, int e) {
-//	LNode* p = L;
-//	while (p != NULL&& p->num != e) {
-//		p = p->next;
-//	}
-//	return p;
-//}
+#include <stdlib.h>
+#include <assert.h>
+int InitLinkList(LinkList*);//初始化
+int InsertTailLinkList(LinkList*);//尾插法
+int InsertHeadLinkList(LinkList*);//头插法
+int InsertList(LinkList,int,int); //插入在第i位
+int InsertPirorList(LNode*, int);//在前一位插入
+int DeleteList(LinkList, int, int*);//删除第i位
+int DeleteNode(LNode*);//定点删除
+LNode* GetElem(LinkList, int);//按位查找
+LNode* LocateElem(LinkList, int);//按值查找
+typedef struct LL {
+	int num;
+	struct LL* next;
+}LNode,*LinkList;
+int main() {
+	LinkList L;
+	return 0;
+}
+int InitLinkList(LinkList* L) {
+	(*L) = (LinkList)malloc(sizeof(LNode));
+	if ((*L) == NULL)
+		return -1;
+	(*L)->next = NULL;
+	(*L)->num = 0;
+	return 0;
+}
+int InsertTailLinkList(LinkList* L) {
+	(*L) = (LinkList)malloc(sizeof(LNode));
+	if ((*L) == NULL)
+		return -1;
+	(*L)->next = NULL;
+	(*L)->num = 0;
+	LNode* r = (*L);
+	LNode* s = (*L);
+	int x = 0;
+	if (scanf("%d", &x) != 1) {
+		return -1;
+	}
+	while (x != 9999) {
+		s = (LNode*)malloc(sizeof(LNode));
+		if (s == NULL)
+			return -1;
+		s->num = x;
+		s->next = r->next;
+		r->next = s;
+		r = s;
+		if (scanf("%d", &x) != 1) {
+			return -1;
+		}
+	}
+	r->next = NULL;
+	return 0;
+}
+int InsertHeadLinkList(LinkList* L) {
+	(*L) = (LinkList)malloc(sizeof(LNode));
+	if ((*L) == NULL)
+		return -1;
+	(*L)->next = NULL;
+	(*L)->num = 0;
+	LNode* s = (*L);
+	int x = 0;
+	if (scanf("%d", &x) != 1) {
+		return -1;
+	}
+	while (x != 9999) {
+		s = (LNode*)malloc(sizeof(LNode));
+		if (s == NULL)
+			return -1;
+		s->num = x;
+		s->next = (*L)->next;
+		(*L)->next = s;
+		if (scanf("%d", &x) != 1) {
+			return -1;
+		}
+	}
+	return 0;
+}
+int InsertList(LinkList L, int i, int e) {
+	assert(L);
+	if (i < 0)
+		return -1;
+	int j = 0;
+	LNode* p = L;
+	while (p != NULL && j < i - 1) {
+		j++;
+		p = p->next;
+	}
+	if (p == NULL)
+		return -1;
+	LNode* s = (LNode*)malloc(sizeof(LNode));
+	if (s == NULL) {
+		return -1;
+	}
+	s->num = e;
+	s->next = p->next;
+	p->next = s;
+	return 0;
+}
+int InsertPirorList(LNode* p, int e) {
+	LNode* s = (LNode*)malloc(sizeof(LNode));
+	//创建新节点t 把p的数据放入t中 把t的数据放入p中
+	if (s == NULL) {
+		return -1;
+	}
+	s->num = p->num;
+	p->num = e;
+	s->next = p->next;
+	p->next = s;
+	return 0;
+}
+int DeleteList(LinkList L, int i, int* e) {
+	assert(L);
+	if (i < 0) {
+		return -1;
+	}
+	int j = 0;
+	LNode* p = L;
+	while (p != NULL && j < i-1) {
+		p = p->next;
+		j++;
+	}
+	if (p == NULL)
+		return -1;
+	LNode* p1 = p->next;
+	p->next = p1->next;
+	*e = p1->num;
+	free(p1);
+	return 0;
+	////若不带头节点
+	//if (i == 1) {
+	//	//此时传参要传LinkList* L否则无法改变L所指向
+	//	LNode* p1 = *L;
+	//	*e = p1->num;
+	//	*L = p1->next;
+	//	free(p1);
+	//	return 0;
+	//}//其他情况与上方一样
+
+}
+int DeleteNode(LNode* p) {
+	assert(p&&p->next);
+	LNode* p1 = p->next;
+	p->num = p1->num;
+	p->next = p1->next;
+	free(p1);
+	return 0;
+}
+LNode* GetElem(LinkList L, int i) {
+	if (i < 0)
+		return NULL;
+	int j = 0;
+	LNode* p = L;
+	while (p != NULL && j < i) {
+		p = p->next;
+		j++;
+	}
+	return p;
+}
+LNode* LocateElem(LinkList L, int e) {
+	LNode* p = L;
+	while (p != NULL && p->num != e) {
+		p = p->next;
+	}
+	return p;
+}
+
 
 ////双链表
 //#include<assert.h>
